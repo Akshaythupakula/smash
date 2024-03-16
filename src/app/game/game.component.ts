50b4dd2404386959eb4ca32716be116db812ca0d
Akshaythupakula <akshaygoud509@gmail.com>
2024-03-16 15:35:21 +0530

    changes

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -12,20 +12,20 @@ import { CommonModule } from '@angular/common';
 })
 export class GameComponent implements OnInit {
   private subscription: Subscription;
-  colors1=''
-  colors2=''
-  colors3=''
-  colors4=''
-  colors5=''
-  colors6=''
-  colors7=''
-  colors8=''
-  colors9=''
-  text=''
-  heatlth=100
-  message=0
-  take=1000
-  colr:string[]=['blue','black','red']
+  colors1 = ''
+  colors2 = ''
+  colors3 = ''
+  colors4 = ''
+  colors5 = ''
+  colors6 = ''
+  colors7 = ''
+  colors8 = ''
+  colors9 = ''
+  text = ''
+  heatlth = 100
+  message = 0
+  take = 1200
+  colr: string[] = ['blue', 'black', 'red']
   constructor() {
     this.subscription = new Subscription(); // Initialize the property in the constructor
   }
@@ -41,7 +41,7 @@ export class GameComponent implements OnInit {
   }
 
   ngOnDestroy(): void {
-    if (this.subscription && this.heatlth<1) {
+    if (this.subscription) {
       this.subscription.unsubscribe(); // Clean up subscription when component is destroyed
     }
   }
@@ -51,31 +51,30 @@ export class GameComponent implements OnInit {
     const randomIndex = Math.floor(Math.random() * this.colr.length);
     const c2 = Math.floor(Math.random() * this.colr.length);
     const c3 = Math.floor(Math.random() * this.colr.length);
-    const text = Math.floor(Math.random() * this.colr.length);
 
-    this.colors1=this.colr[randomIndex]
-    this.colors2=this.colr[c2]
-    this.colors3=this.colr[c3]
+    this.colors1 = this.colr[randomIndex]
+    this.colors2 = this.colr[c2]
+    this.colors3 = this.colr[c3]
 
 
-    this.text=this.colr[text]
-    if(this.message==0 && this.heatlth==0){
-      this.take=0
-      this.colors1='white'
-    this.colors2='white'
-    this.colors3='white'
+    
+    if ( this.heatlth < 1) {
+      this.take = 0
+      this.colors1 = 'white'
+      this.colors2 = 'white'
+      this.colors3 = 'white'
+      this.text = 'game over'
 
     }
   }
-  smash(event:string){
-    if(event=='blue'){
-      this.message=this.message+10
+  smash(event: string) {
+    if (event == 'blue') {
+      this.message = this.message + 10
       console.log(event)
-      this.take=this.take+10
 
-    }else if(event!='blue'){
-      this.message=this.message-10
-      this.heatlth=this.heatlth-10
+    } else if (event != 'blue') {
+      this.message = this.message - 10
+      this.heatlth = this.heatlth - 10
     }
     console.log(event)
   }
