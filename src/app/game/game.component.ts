3ba5445c23cdb00b54d05e5a0128054c675619bf
Akshaythupakula <akshaygoud509@gmail.com>
2024-03-16 18:36:29 +0530

    mobile view

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -22,18 +22,19 @@ export class GameComponent implements OnInit {
   colors8 = ''
   colors9 = ''
   text = ''
-  heatlth = 100
+  heatlth = 10
   message = 0
-  take = 20
-  colr: string[] = ['blue', 'black', 'red']
+  take=10
+  
+  colr: string[] = ['blue', 'black', 'red','white','white','white','white','white','white',]
   constructor() {
     this.subscription = new Subscription(); // Initialize the property in the constructor
   }
 
   ngOnInit(): void {
-    this.subscription = interval(1000) // 3000 milliseconds = 3 seconds
+    this.subscription = interval(3000) 
       .pipe(
-        take(this.take) // Run the function 10 times (for demonstration purposes)
+        take(this.take) 
       )
       .subscribe(() => {
         this.myFunction(); // Call your function here
@@ -46,40 +47,54 @@ export class GameComponent implements OnInit {
     }
   }
 
+
+
+
   myFunction(): void {
 
     const randomIndex = Math.floor(Math.random() * this.colr.length);
     const c2 = Math.floor(Math.random() * this.colr.length);
     const c3 = Math.floor(Math.random() * this.colr.length);
+    const c4 = Math.floor(Math.random() * this.colr.length);
+    const c5 = Math.floor(Math.random() * this.colr.length);
+    const c6 = Math.floor(Math.random() * this.colr.length);
+    const c7 = Math.floor(Math.random() * this.colr.length);
+    const c8 = Math.floor(Math.random() * this.colr.length);
+    const c9 = Math.floor(Math.random() * this.colr.length);
 
     this.colors1 = this.colr[randomIndex]
     this.colors2 = this.colr[c2]
     this.colors3 = this.colr[c3]
+    this.colors4 = this.colr[c4]
+    this.colors5 = this.colr[c5]
+    this.colors6 = this.colr[c6]
+    this.colors7 = this.colr[c7]
+    this.colors8 = this.colr[c8]
+    this.colors9 = this.colr[c9]
 
 
 
-    if (this.heatlth < 1) {
-      this.take = 0
+ 
+  }
+  smash(event: string) {
+    if (event == 'blue'&& this.heatlth >1) {
+      this.message = this.message + 10
+
+
+    } else if (event != 'blue'&& this.heatlth > 1) {
+      this.message = this.message - 10
+      this.heatlth = this.heatlth - 1
+
+    }
+    else if(this.heatlth < 1){
+      this.message = this.message
       this.colors1 = 'white'
       this.colors2 = 'white'
       this.colors3 = 'white'
       this.text = 'game over'
-
-    }
-  }
-  smash(event: string) {
-    if (event == 'blue') {
-      this.message = this.message + 10
       this.take=this.heatlth
 
-    } else if (event != 'blue') {
-      this.message = this.message - 10
-      this.heatlth = this.heatlth - 10
-      this.take=this.heatlth
-
-    }
-    else if (this.heatlth < 1) {
-      this.message = this.message
+      // this.subscription.unsubscribe();
 
     }
   }
